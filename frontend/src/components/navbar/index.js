8d99a897cdb61ae151b4a27b34055cbb8e39fa53
sai prakash <saiprakash@nvestbank.com>
2020-12-03 15:39:16 +0530

    initial commit

---
+++ frontend/src/components/navbar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => (
+    <nav>
+        <div className="nav-wrapper purple darken-4">
+        <a href="/" className="brand-logo center">Welcome, Here you can Add, Edit, Delete Users in the list</a>
+        </div>
+    </nav>
+);
+
+export default NavBar;
